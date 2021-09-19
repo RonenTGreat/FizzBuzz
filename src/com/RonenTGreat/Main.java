c7e18d9b8a501c2462f7a7c1b0f3775454605c38
ronentgreat <ronenh53@gmail.com>
2021-09-19 18:31:51 +0000

    start

---
+++ src/com/RonenTGreat/Main.java
@@ -0,0 +1,14 @@
+package com.RonenTGreat;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int number;
+        System.out.print("Please enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        number = scanner.nextInt();
+        System.out.println(number);
+    }
+}

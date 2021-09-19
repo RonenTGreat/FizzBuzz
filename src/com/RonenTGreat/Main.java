9bc4770a8d01843488348ccb4c6cc6f31a68d18a
ronentgreat <ronenh53@gmail.com>
2021-09-19 18:39:11 +0000

    start

--- src/com/RonenTGreat/Main.java
+++ src/com/RonenTGreat/Main.java
@@ -9,6 +9,17 @@ public class Main {
         System.out.print("Please enter a number: ");
         Scanner scanner = new Scanner(System.in);
         number = scanner.nextInt();
-        System.out.println(number);
+
+        for(int i = 1; i <= number; i++){
+            if(i % 5 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0){
+
+            }
+        }
     }
 }

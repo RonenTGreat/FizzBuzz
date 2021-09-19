24a55a380172c9a47bc66570273fb586396809f2
ronentgreat <ronenh53@gmail.com>
2021-09-19 18:43:52 +0000

    start

--- src/com/RonenTGreat/Main.java
+++ src/com/RonenTGreat/Main.java
@@ -9,16 +9,19 @@ public class Main {
         System.out.print("Please enter a number: ");
         Scanner scanner = new Scanner(System.in);
         number = scanner.nextInt();
+        System.out.println("You enter " + number + ".\n");
 
         for(int i = 1; i <= number; i++){
-            if(i % 5 == 0 && i % 5 == 0){
+            if(i % 5 == 0 && i % 3 == 0){
                 System.out.println("FizzBuzz");
             }
             else if(i % 3 == 0){
                 System.out.println("Fizz");
             }
             else if(i % 5 == 0){
-
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
             }
         }
     }
